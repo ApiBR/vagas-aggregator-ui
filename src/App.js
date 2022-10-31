9805bff125a2aba3e36e752dfea9b32d13d5467f
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-01 02:29:15 +0400

    Add rel to links

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
         <Repositories repositories={this.state.repositories}/>
         <footer className="font-small text-center mt-4 mb-3">
           Desenvolvido por 
-          <a href="https://guibranco.github.io/?utm_campaign=project&utm_media=vagas+aggregator&utm_source=apibr.com">
+          <a href="https://guibranco.github.io/?utm_campaign=project&utm_media=vagas+aggregator&utm_source=apibr.com" target="_blank" rel="noopener noreferrer">
             &nbsp; 
             <img src="https://guibranco.github.io/photo.png" alt="Guilherme Branco Stracini" className="image-rounded image-responsive" style={{ width: "24px" }} />
             &nbsp;
