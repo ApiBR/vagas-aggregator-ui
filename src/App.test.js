6abf73267ccd015f3857c2f061932a202eb3aaaf
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-04 21:17:47 +0400

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Vagas and Repositórios alerts', () => {
+test('renders Vagas alert', () => {
   render(<App />);
   const vagasElement = screen.getByText(/Vagas: 0/i);
   expect(vagasElement).toBeInTheDocument();
-
-  const repositoriosElement = screen.getByText(/Repositórios: 0/i);
-  expect(repositoriosElement).toBeInTheDocument();
 });
