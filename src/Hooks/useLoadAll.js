a3d8ddb60faf41bf1f54d3b9028ccd7f720437b8
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-04 20:45:45 +0400

    Fix load all pages

--- src/Hooks/useLoadAll.js
+++ src/Hooks/useLoadAll.js
@@ -1,24 +1,27 @@
 import { useState, useEffect, useRef } from "react";
-import  useFetch from "./useFetch";
+import useFetch from "./useFetch";
 
 export default function useLoadAll(entity, params) {
   const entityRef = useRef(entity);
   const paramsRef = useRef(params);
   const [items, setItems] = useState([]);
   const [page, setPage] = useState(1);
-  const [state, loadPage] = useFetch(entityRef.current, paramsRef.current, page);
+  const [state, loadPage] = useFetch(
+    entityRef.current,
+    paramsRef.current,
+    page
+  );
 
   useEffect(() => {
-    if(state.loading)
-      return;    
-    if(state.items.length > 0){
-      setItems(items => [...items, ...state.items]);
+    if (state.loading) return;
+    if (state.items.length > 0) {
+      setItems((items) => [...items, ...state.items]);
     }
-    if(page === 1 || state.hasMorePages){
-      loadPage();  
+    if (page === 1 || page <= state.pageCount) {
       setPage(page + 1);
+      setTimeout(loadPage, 100);
     }
   }, [page, state, loadPage]);
 
   return items;
-};
+}
