d877a23bd6d962b5a8b180ec2c78a06cbe42e8a4
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-04 21:31:30 +0400

    Add CookieConsent

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
 
@@ -8,6 +9,16 @@ const Layout = () => {
       <NavBar />
       <Outlet />
       <Footer />
+      <CookieConsent
+        location="bottom"
+        buttonText="Aceito"
+        cookieName="consentCookie"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        Este site usa cookies de rastreamento do Google Analytics.
+      </CookieConsent>
     </div>
   );
 };
