25a8e8c9afe6ccb0c5592d7d005987f96c8455cb
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-04 18:14:46 +0400

    Update logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,11 @@
+import logo from "../ApiBRLogo.png";
+
 export const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="https://apibr.com/ui/">API BR</a>
+            <a className="navbar-brand" href="https://apibr.com/ui/">
+                <img src={logo} alt="API BR Logo" style={{ width: "48px", marginLeft: "10px"}} />
+            </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
