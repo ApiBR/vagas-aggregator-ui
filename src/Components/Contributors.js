9fb692b64ad3792808da428cc878a4d3549120a0
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-04 10:24:56 +0400

    Adjust margin

--- src/Components/Contributors.js
+++ src/Components/Contributors.js
@@ -1,7 +1,7 @@
 export const Contributor = ({ contributor, showName = false }) => {
     const name = showName ? (<span>{contributor.login}</span>) : "";
     return (
-        <div className="mb-2 mr-2">
+        <div className="mb-3 mt-3 col-lg-1">
             <a href={contributor.url} target="_blank" title={contributor.login} rel="noopener noreferrer">
                 <img src={contributor.avatar_url} alt={contributor.login} className="rounded-circle img-responsive" style={{width: "48px"}}/>
                 {name}
@@ -15,7 +15,7 @@ export const Contributors = ({ contributors }) => {
         return (<Contributor contributor={contributor} key={contributor.id} />);
     });
     return (
-        <div className="row">
+        <div className="d-flex row">
             {contributorsItems}
         </div>
     )
