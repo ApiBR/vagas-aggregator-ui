2b8594758eb27173bdbfcb306bfc38ec551d1451
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-05 05:50:02 +0400

    Add text to NovaVaga

--- src/Pages/NovaVaga.js
+++ src/Pages/NovaVaga.js
@@ -1,5 +1,15 @@
+import {Link } from "react-router-dom";
+
 const NovaVaga = () => {
-    return (<h1>Nova vaga</h1>);
-}
+  return (
+    <div className="alert alert-info text-center">
+      Para publicar uma nova vaga, crie uma conta no{" "}
+      <a href="https://github.com">GitHub</a> e então localize o repositório
+      desejado, e siga as regras de publicação de cada repositório.
+      <br />
+      O link dos repositórios podem ser encontrados na página de <Link to="/repositorios">repositórios</Link> deste projeto.
+    </div>
+  );
+};
 
 export default NovaVaga;
