946d9059b424ae56b6bbfdd744f4873f326cf390
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-06 02:52:39 +0400

    Change logo wi=dth

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <a className="navbar-brand" href="https://apibr.com/ui/">
-                <img src={logo} alt="API BR Logo" style={{ width: "48px", marginLeft: "10px"}} />
+                <img src={logo} alt="API BR Logo" style={{ width: "80px", marginLeft: "10px"}} />
             </a>
             <div>
                 <ul className="navbar-nav me-auto">
