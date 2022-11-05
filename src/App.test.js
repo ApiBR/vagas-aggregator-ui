f0409454676330e46993840cf28bb01faaa63d86
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-06 01:05:14 +0400

    Add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -16,9 +16,23 @@ test("renders Repositórios alert", () => {
   expect(vagasElement).toBeInTheDocument();
 });
 
-test("renders Nova Vaga h1", () => {
+test("renders Recrutadores alert", () => {
+  window.history.pushState({}, '', '/ui/vagas/recrutadores')
+  render(<App />);
+  const vagasElement = screen.getByText(/Recrutadores: 0/i);
+  expect(vagasElement).toBeInTheDocument();
+});
+
+test("renders Nova vaga alert", () => {
   window.history.pushState({}, '', '/ui/vagas/nova-vaga')
   render(<App />);
-  const vagasElement = screen.getByText(/Nova Vaga/i);
+  const vagasElement = screen.getByText(/nova vaga/i);
+  expect(vagasElement).toBeInTheDocument();
+});
+
+test("renders footer", () => {
+  window.history.pushState({}, '', '/ui/vagas/')
+  render(<App />);
+  const vagasElement = screen.getByText(/Desenvolvido por/i);
   expect(vagasElement).toBeInTheDocument();
 });
