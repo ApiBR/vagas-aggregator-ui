3b1439c86df9165dea55ad4e1c59e77d3f6b5d1c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-06 22:34:59 +0400

    Fix placeholder resposivity

--- src/Components/Placeholder.js
+++ src/Components/Placeholder.js
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 
 export const Item = () => {
   return (
-    <div className="card border-default placeholder-glow mb-3 col-lg-3">
+    <div className="card border-default placeholder-glow mb-3 col-xs-12 col-lg-3">
       <div className="card-header">
         <img
           src="https://via.placeholder.com/48?text=%20"
@@ -10,16 +10,16 @@ export const Item = () => {
           className="rounded-circle img-responsive"
           style={{ width: "48px", marginRight: "20px" }}
         />
-        <span className="placeholder col-lg-6"></span>
+        <span className="placeholder col-6"></span>
       </div>
       <div className="card-body">
         <div className="row">
-          <div className="col-lg-10">
-            <span className="placeholder col-lg-12"></span>
-            <span className="placeholder col-lg-12"></span>
-            <span className="placeholder col-lg-12"></span>
+          <div className="col-10">
+            <span className="placeholder col-12"></span>
+            <span className="placeholder col-12"></span>
+            <span className="placeholder col-12"></span>
           </div>
-          <div className="col-lg-2">
+          <div className="col-2">
             <img
               src="https://via.placeholder.com/48?text=%20"
               alt="placeholder"
@@ -28,39 +28,39 @@ export const Item = () => {
             />
           </div>
         </div>
-        <span className="placeholder col-lg-6 pb-2 pt-2 mr-1"></span>
+        <span className="placeholder col-6 pb-2 pt-2 mr-1"></span>
         <br />
-        <span className="placeholder col-lg-6 pb-2 pt-2"></span>
-        <div className="alert alert-warning mt-3 mb-3 placeholder col-lg-12"></div>
+        <span className="placeholder col-6 pb-2 pt-2"></span>
+        <div className="alert alert-warning mt-3 mb-3 placeholder col-12"></div>
       </div>
       <div className="card-footer">
         <div className="row">
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(180, 252, 160)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(180, 252, 160)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(170, 100, 100)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(170, 100, 100)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(170, 100, 100)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(170, 100, 100)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(100, 155, 100)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(100, 155, 100)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(205, 234, 220)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(205, 234, 220)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(8, 127, 91)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(8, 127, 91)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(252, 156, 4)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(252, 156, 4)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(173, 121, 233)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(173, 121, 233)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
-          <span className="badge mr-2 mb-2 col-lg-1" style={{border: "2px solid rgb(15, 144, 220)", color: "#FFF"}}>
-              <span className="placeholder col-lg-12"></span>
+          <span className="badge mr-2 mb-2 col-1" style={{border: "2px solid rgb(15, 144, 220)", color: "#FFF"}}>
+              <span className="placeholder col-12"></span>
           </span>
         </div>
       </div>
