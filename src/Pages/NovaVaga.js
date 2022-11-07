ef9f7ec656a45e15084e83b60dbb168b4c7a6257
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-07 20:39:13 +0400

    Add margin top

--- src/Pages/NovaVaga.js
+++ src/Pages/NovaVaga.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NovaVaga = () => {
   return (
-    <div className="alert alert-info text-center">
+    <div className="alert alert-info text-center mt-4">
       Para publicar uma nova vaga, crie uma conta no{" "}
       <a href="https://github.com">GitHub</a> e então localize o repositório
       desejado, e siga as regras de publicação de cada repositório.
