ff9abc4762fc9dc9c802242de91654d778a43619
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-14 05:20:21 +0400

    Update ContributorItem.js

--- src/Components/Contributors/ContributorItem.js
+++ src/Components/Contributors/ContributorItem.js
@@ -1,7 +1,7 @@
 const ContributorItem = ({ contributor, showName = false }) => {
   const name = showName ? <span>{contributor.login}</span> : "";
   return (
-    <div className="mb-3 mt-3 col-xs-2 col-lg-1">
+    <div className="mb-3 mt-3 col-1">
       <a
         href={contributor.url}
         target="_blank"
