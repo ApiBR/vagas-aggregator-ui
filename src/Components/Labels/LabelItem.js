c3ad05915df17281603b3310dd91f0835d153360
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-11-28 16:18:35 +0400

    Fix space in labels

--- src/Components/Labels/LabelItem.js
+++ src/Components/Labels/LabelItem.js
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 const LabelItem = ({ label }) => {
   const url = "/?labels=" + label.name;
   return (
-    <Link to={url}>
-      <span
-        className="badge mr-2 mb-2"
-        style={{
-          textTransform: "capitalize",
-          border: "2px solid #" + label.color,
-          color: "#FFF",
-        }}
-      >
-        {label.name}
-      </span>{" "}
-    </Link>
+    <>
+      <Link to={url}>
+        <span
+          className="badge mr-2 mb-2"
+          style={{
+            textTransform: "capitalize",
+            border: "2px solid #" + label.color,
+            color: "#FFF",
+          }}
+        >
+          {label.name}
+        </span>
+      </Link>{" "}
+    </>
   );
 };
 
