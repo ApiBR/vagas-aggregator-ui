f299b3b9a83204cc5347c069e3501f4e35c331b9
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-08-12 16:44:36 +0100

    Fix warnings

--- src/Hooks/useMetadata.js
+++ src/Hooks/useMetadata.js
@@ -10,25 +10,26 @@ export default function useMetadata() {
         date: "--/--/----"
     });
 
-    const cancelToken = axios.CancelToken.source();
+
 
     useEffect(() => {
-    axios.head(BASE_URL, {
-        cancelToken: cancelToken.token
-    }).then((res) => {
-        const date = new Date(res.headers["x-api-date"]);
-        const mm = date.getMonth() + 1;
-        const d = date.getDate();
-        const data = {
-            version: res.headers["x-api-version"],
-            date: [(d > 9 ? "" : "0") + d, (mm > 9 ? "" : "0") + mm, date.getFullYear()].join("/")
-        };
-        
-        setMetadata(data);
-    })
-    .catch((err) => {
-        toastr.error("Não foi possível obter os metadados da API.");
-    });
+        const cancelToken = axios.CancelToken.source();
+        axios.head(BASE_URL, {
+            cancelToken: cancelToken.token
+        }).then((res) => {
+            const date = new Date(res.headers["x-api-date"]);
+            const mm = date.getMonth() + 1;
+            const d = date.getDate();
+            const data = {
+                version: res.headers["x-api-version"],
+                date: [(d > 9 ? "" : "0") + d, (mm > 9 ? "" : "0") + mm, date.getFullYear()].join("/")
+            };
+
+            setMetadata(data);
+        })
+            .catch((err) => {
+                toastr.error("Não foi possível obter os metadados da API.");
+            });
     }, []);
 
     return metadata;
