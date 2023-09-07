3f86b59c2de62848d50096e5300fa090e02880a7
Anurav Modak <>
2023-09-07 22:59:36 +0530

    missing_error_handling_v1

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<App />);
 reportWebVitals(sendToGoogleAnalytics);
 
