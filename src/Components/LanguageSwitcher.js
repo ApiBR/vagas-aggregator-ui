4f48b66d4cb27ed43fbdd198f21e5148cf7c05ec
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-07-16 01:47:54 +0100

    Update LanguageSwitcher.js

--- src/Components/LanguageSwitcher.js
+++ src/Components/LanguageSwitcher.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
@@ -9,8 +9,9 @@ const LanguageSwitcher = () => {
 
   return (
     <div>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button onClick={() => changeLanguage('es')}>Español</button>
+      <button onClick={() => changeLanguage("pt")}>Português</button>
+      <button onClick={() => changeLanguage("en")}>English</button>
+      <button onClick={() => changeLanguage("es")}>Español</button>
     </div>
   );
 }
