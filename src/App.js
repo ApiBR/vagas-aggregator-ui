66860cb967a3bfa9839f0db8d6429092c54f0422
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-04-14 15:56:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from "./Pages/Layout";
 import Vagas from "./Pages/Vagas";
 import Repositorios from "./Pages/Repositorios";
@@ -10,22 +10,16 @@ import ErrorBoundary from "./Helpers/ErrorBoundary";
 function App() {
   return (
     <ErrorBoundary>
-      <BrowserRouter
-        basename={"/ui/vagas"}
-        future={{
-          v7_startTransition: true,
-          v7_relativeSplatPath: true,
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Vagas />} />
-            <Route path="repositorios" element={<Repositorios />} />
-            <Route path="recrutadores" element={<Recrutadores />} />
-            <Route path="nova-vaga" element={<NovaVaga />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Router basename="/ui/vagas">
+        <Layout>
+            <Routes>
+              <Route path="/" element={<Vagas />} />
+              <Route path="repositorios" element={<Repositorios />} />
+              <Route path="recrutadores" element={<Recrutadores />} />
+              <Route path="nova-vaga" element={<NovaVaga />} />
+            </Routes>
+        </Layout>
+      </Router>
     </ErrorBoundary>
   );
 }
