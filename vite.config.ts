2776d5872a55ae48046c187554e7ff4f066548c4
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-03-25 13:49:17 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/ui/vagas/",
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
